5731f9bc42bfb2b456022dec8067f26cc7e7bf46
WilliamSongLiu <williamsongliu@gmail.com>
2022-01-20 01:05:38 -0800

    new sage strategy

--- src/m10/SageStrategy.java
+++ src/m10/SageStrategy.java
@@ -5,6 +5,8 @@ import battlecode.common.*;
 import java.util.Arrays;
 
 strictfp class SageStrategy {
+    static final double CHARGE_DAMAGE = 0.22;
+    static final double FURY_DAMAGE = 0.1;
 
     /** Called by RobotPlayer */
     static void runSage() throws GameActionException {
@@ -27,42 +29,128 @@ strictfp class SageStrategy {
             RobotInfo[] enemies = RobotPlayer.rc.senseNearbyRobots(GeneralManager.myType.actionRadiusSquared, GeneralManager.enemyTeam);
             MapLocation[] leadLocations = RobotPlayer.rc.senseNearbyLocationsWithLead(GeneralManager.myType.actionRadiusSquared);
 
-            int allyArchonCount = 0, allyDroidCount = 0, enemyArchonCount = 0, enemyDroidCount = 0;
-            for (RobotInfo ally : allies) {
-                if (Arrays.asList(GeneralManager.DROIDS).contains(ally.getType())) allyDroidCount++;
-                if (ally.getType().equals(RobotType.ARCHON)) allyArchonCount++;
-            }
+            // Paige's version
+//            int allyArchonCount = 0, allyDroidCount = 0, enemyArchonCount = 0, enemyDroidCount = 0;
+//            for (RobotInfo ally : allies) {
+//                if (Arrays.asList(GeneralManager.DROIDS).contains(ally.getType())) allyDroidCount++; // I changed this to check for droids like the variable's name, not sure if you intended to check for combat droids only lol
+//                if (ally.getType().equals(RobotType.ARCHON)) allyArchonCount++;
+//            }
+//            for (RobotInfo enemy : enemies) {
+//                if (Arrays.asList(GeneralManager.DROIDS).contains(enemy.getType())) enemyDroidCount++;
+//                if (enemy.getType().equals(RobotType.ARCHON)) enemyArchonCount++;
+//            }
+//
+//            if (enemyArchonCount >= 2 && allyArchonCount == 0 && RobotPlayer.rc.canEnvision(AnomalyType.FURY)) {
+//                RobotPlayer.rc.envision(AnomalyType.FURY);
+//            }
+//            else if ((CombatManager.evaluateLocalCombatScore(GeneralManager.myTeam, false) <
+//                    CombatManager.evaluateLocalCombatScore(GeneralManager.enemyTeam, true) ||
+//                    enemyDroidCount > 6 || (enemyDroidCount > 1 && RobotPlayer.rc.getHealth() <= 30)) &&
+//                    RobotPlayer.rc.canEnvision(AnomalyType.CHARGE)) {
+//                RobotPlayer.rc.envision(AnomalyType.CHARGE);
+//            }
+//            else if (enemyArchonCount >= 1 && allyArchonCount == 0 && RobotPlayer.rc.canEnvision(AnomalyType.FURY)) {
+//                RobotPlayer.rc.envision(AnomalyType.FURY);
+//            }
+//            else {
+//                if (ArchonTrackerManager.getCentralEnemyArchon() != -1)
+//                    GeneralManager.tryMove(getSageDirToEncircle(ArchonTrackerManager.enemyArchonTrackers[ArchonTrackerManager.getCentralEnemyArchon()].getGuessLocation(),
+//                        4), false);
+//                else
+//                    GeneralManager.tryMove(GeneralManager.DIRECTIONS[GeneralManager.rng.nextInt(GeneralManager.DIRECTIONS.length)], true);
+//
+//                if (ArchonTrackerManager.getNearestEnemyArchonGuessLocation(GeneralManager.myLocation) != null &&
+//                        GeneralManager.myLocation.distanceSquaredTo(ArchonTrackerManager.getNearestAllyArchonLocation(GeneralManager.myLocation))
+//                    > GeneralManager.myLocation.distanceSquaredTo(ArchonTrackerManager.getNearestEnemyArchonGuessLocation(GeneralManager.myLocation))) {
+//                    if (leadLocations.length > 2 && RobotPlayer.rc.canEnvision(AnomalyType.ABYSS)) {
+//                        RobotPlayer.rc.envision(AnomalyType.ABYSS);
+//                    }
+//                }
+//            }
+
+            // William's version
+            int chargeScore = 0, furyScore = 0;
             for (RobotInfo enemy : enemies) {
-                if (Arrays.asList(GeneralManager.DROIDS).contains(enemy.getType())) enemyDroidCount++;
-                if (enemy.getType().equals(RobotType.ARCHON)) enemyArchonCount++;
+                // Charge
+                if (Arrays.asList(GeneralManager.DROIDS).contains(enemy.getType())) {
+                    int health = enemy.getHealth();
+                    int maxHealth = enemy.getType().getMaxHealth(enemy.level);
+                    int damage = (int)(CHARGE_DAMAGE * maxHealth); // Not sure if the floor is correct here
+                    health -= damage;
+                    if (health < 0) {
+                        switch (enemy.getType()) {
+                            case SAGE: chargeScore += 100; break;
+                            case SOLDIER: chargeScore += 50; break;
+                            case MINER: chargeScore += 20; break;
+                            case BUILDER: chargeScore += 10; break;
+                        }
+                    }
+                    else {
+                        chargeScore += damage;
+                        switch (enemy.getType()) {
+                            case SAGE: chargeScore += 10; break;
+                            case SOLDIER: chargeScore += 5; break;
+                            case MINER: chargeScore += 2; break;
+                            case BUILDER: chargeScore += 0; break;
+                        }
+                    }
+                }
+                // Fury
+                else if (enemy.getMode() == RobotMode.TURRET) {
+                    int health = enemy.getHealth();
+                    int maxHealth = enemy.getType().getMaxHealth(enemy.level);
+                    int damage = (int)(FURY_DAMAGE * maxHealth); // Not sure if the floor is correct here
+                    health -= damage;
+                    if (health < 0) {
+                        switch (enemy.getType()) {
+                            case ARCHON: furyScore += 10000; break;
+                            case WATCHTOWER: furyScore += 75; break;
+                            case LABORATORY: furyScore += 30; break;
+                        }
+                    }
+                    else {
+                        chargeScore += damage;
+                        switch (enemy.getType()) {
+                            case ARCHON: furyScore += 2; break;
+                            case WATCHTOWER: furyScore += 5; break;
+                            case LABORATORY: furyScore += 0; break;
+                        }
+                    }
+                }
+            }
+            for (RobotInfo ally : allies) {
+                // Fury
+                if (ally.getMode() == RobotMode.TURRET) {
+                    int health = ally.getHealth();
+                    int maxHealth = ally.getType().getMaxHealth(ally.level);
+                    int damage = (int)(FURY_DAMAGE * maxHealth); // Not sure if the floor is correct here
+                    health -= damage;
+                    if (health < 0) {
+                        switch (ally.getType()) {
+                            case ARCHON: furyScore -= 10000; break;
+                            case WATCHTOWER: furyScore -= 75; break;
+                            case LABORATORY: furyScore -= 30; break;
+                        }
+                    }
+                    else {
+                        chargeScore -= damage;
+                        switch (ally.getType()) {
+                            case ARCHON: furyScore -= 2; break;
+                            case WATCHTOWER: furyScore -= 5; break;
+                            case LABORATORY: furyScore -= 0; break;
+                        }
+                    }
+                }
             }
 
-            if (enemyArchonCount >= 2 && allyArchonCount == 0 && RobotPlayer.rc.canEnvision(AnomalyType.FURY)) {
-                RobotPlayer.rc.envision(AnomalyType.FURY);
-            }
-            else if ((CombatManager.evaluateLocalCombatScore(GeneralManager.myTeam, false) <
-                    CombatManager.evaluateLocalCombatScore(GeneralManager.enemyTeam, true) ||
-                    enemyDroidCount > 6 || (enemyDroidCount > 1 && RobotPlayer.rc.getHealth() <= 30)) &&
-                    RobotPlayer.rc.canEnvision(AnomalyType.CHARGE)) {
+            if (chargeScore > furyScore && chargeScore > 200 && RobotPlayer.rc.canEnvision(AnomalyType.CHARGE)) {
                 RobotPlayer.rc.envision(AnomalyType.CHARGE);
             }
-            else if (enemyArchonCount >= 1 && allyArchonCount == 0 && RobotPlayer.rc.canEnvision(AnomalyType.FURY)) {
+            else if (furyScore > 200 && RobotPlayer.rc.canEnvision(AnomalyType.FURY)) {
                 RobotPlayer.rc.envision(AnomalyType.FURY);
             }
             else {
-                if (ArchonTrackerManager.getCentralEnemyArchon() != -1)
-                    GeneralManager.tryMove(getSageDirToEncircle(ArchonTrackerManager.enemyArchonTrackers[ArchonTrackerManager.getCentralEnemyArchon()].getGuessLocation(),
-                        4), false);
-                else
-                    GeneralManager.tryMove(GeneralManager.DIRECTIONS[GeneralManager.rng.nextInt(GeneralManager.DIRECTIONS.length)], true);
-
-                if (ArchonTrackerManager.getNearestEnemyArchonGuessLocation(GeneralManager.myLocation) != null &&
-                        GeneralManager.myLocation.distanceSquaredTo(ArchonTrackerManager.getNearestAllyArchonLocation(GeneralManager.myLocation))
-                    > GeneralManager.myLocation.distanceSquaredTo(ArchonTrackerManager.getNearestEnemyArchonGuessLocation(GeneralManager.myLocation))) {
-                    if (leadLocations.length > 2 && RobotPlayer.rc.canEnvision(AnomalyType.ABYSS)) {
-                        RobotPlayer.rc.envision(AnomalyType.ABYSS);
-                    }
-                }
+                action = CombatManager.COMBAT_DROID_ACTIONS.HOLD;
             }
         }
         else if (action == CombatManager.COMBAT_DROID_ACTIONS.RETREAT) {
